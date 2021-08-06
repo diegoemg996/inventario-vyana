ea8ca66930043d21d3ddd6e128766efef7090c51
Diego Mendoza <diegoemg996@gmail.com>
2021-08-06 11:56:04 -0500

    Loadin agregado

--- src/pages/inicio/componentes/Tabla.js
+++ src/pages/inicio/componentes/Tabla.js
@@ -5,6 +5,7 @@ import {Busqueda} from './Busqueda';
 import { useForm } from '../../../hooks/useForm';
 import {filtroBodega} from '../../../helpers/filtroBodega';
 import {filtroNombre} from '../../../helpers/filtroNombre'
+import { Loading } from '../../../shared/Loading';
 
 
 export const Tabla = () => {
@@ -34,43 +35,53 @@ export const Tabla = () => {
     }, [nombre, productos, bodega]);
 
     return (
-        <div className="container">
+        <>
 
-            <Busqueda
-                values={values}
-                handleInputChange={handleInputChange}
-                showSearch = {showSearch}
-                setShowSearch = {setShowSearch}
-                showAdd = {showAdd}
-                setShowAdd = {setShowAdd}
-            />
+             {
+                productos.length <= 0 
+                ?
+                <Loading/>
+                :
+                <div className="container animate__animated animate__fadeIn animate__slow">
+                    <Busqueda
+                        values={values}
+                        handleInputChange={handleInputChange}
+                        showSearch = {showSearch}
+                        setShowSearch = {setShowSearch}
+                        showAdd = {showAdd}
+                        setShowAdd = {setShowAdd}
+                    />
 
-            <div className={ (showSearch === false && showAdd === false) ? "tabla" : (showSearch === true && showAdd === true ? 'tabla-chica': 'tabla-mediana')}>
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">Producto</th>
-                            <th scope="col">Bodega</th>
-                            <th scope="col">Cantidad</th>
-                            <th scope="col">Movimientos</th>
-                            <th scope="col">Editar</th>
-                            <th scope="col">Eliminar</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            filtroProductos.map(producto =>(
-                                <ItemTabla
-                                    key={producto._id}
-                                    producto={producto}
-                                />    
-                            ))
-                        }
+                    <div className={ (showSearch === false && showAdd === false) ? "tabla" : (showSearch === true && showAdd === true ? 'tabla-chica': 'tabla-mediana')}>
+                        <table className="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Producto</th>
+                                    <th scope="col">Bodega</th>
+                                    <th scope="col">Cantidad</th>
+                                    <th scope="col">Movimientos</th>
+                                    <th scope="col">Editar</th>
+                                    <th scope="col">Eliminar</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    filtroProductos.map(producto =>(
+                                        <ItemTabla
+                                            key={producto._id}
+                                            producto={producto}
+                                        />    
+                                    ))
+                                }
 
-                    </tbody>
-                </table>
-            </div>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+            }
+ 
             
-        </div>
+        </>
     )
 }
